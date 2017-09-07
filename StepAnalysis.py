a93aa693466b1d8037056124fd902c367271cf79
will wade <willwade@gmail.com>
2017-09-07 21:34:06 +0100

    updated for better calc of lowtech space

--- StepAnalysis.py
+++ StepAnalysis.py
@@ -6,29 +6,49 @@ import re
 @click.command()
 @click.option('--ssteps', type=click.Path(exists=True), default='scan-steps-lib/ssteps-eardu.csv', help='Path to a csv of your scan steps')
 @click.option('--scanrate', default=1000, help='Scan rate in ms')
+@click.option('--output-type', default='all', help='All, Lesher, Damper, Steps, Hits, show-workings, show-predictions, csv-all')
 @click.option('--ignore-spaces', default=True, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
+@click.option('--ignore-predictions', default=False, type=bool, help='Ignore predictions? i.e. ignore anything uppercased')
+@click.option('--prediction-time', default=0, help='How long does it take the person to select a prediction on average? in ms. NB: Ignored if ignored-predictions is True')
 @click.option('--sentence', prompt='Test sentence:',
 				help='Enter your test sentence here')
 
-def stepcount(ssteps, scanrate, ignore_spaces ,sentence):
+def stepcount(ssteps, scanrate, ignore_spaces , ignore_predictions, prediction_time, sentence, output_type):
 	"""Takes a switch step count and a sentence and display number of steps to get there"""
 	letterfreq = dict()
-	sum = 0
+	sum = t_lesher = t_damper = sum_pred_letters = sum_pred_words = sum_words = 0
+	show_workings = ''
 	# remove dodgy chars
 	# Bad code. Could do this a lot better if I spent 5 minutes 
-	s_filtered = re.sub(r"[0-9]+", '', sentence.lower())
+	s_filtered = re.sub(r"[0-9]+", '', sentence)
 	s_filtered = re.sub(r"#+", '', s_filtered)
 	s_filtered = re.sub(r"'+", '', s_filtered)
 	s_filtered = re.sub(r"\/+", '', s_filtered)
-	s_filtered2 = re.sub(r"\s+", '', s_filtered)
 
+	# predictions?
+	for word in s_filtered.split():
+		len_ucase_str = len(re.findall(r'[A-Z]',word))
+		sum_pred_letters = sum_pred_letters + len_ucase_str
+		sum_pred_words = sum_pred_words +1 if len_ucase_str > 0  else sum_pred_words
+	
+	# ignore predictions?
+	if ignore_predictions:
+		s_filtered = re.sub(r"[A-Z]+", '', s_filtered)
+
+	s_filtered = s_filtered.lower()
+	
+	#calc word count
+	sum_words = len(re.findall(r"\s+",s_filtered))+1
+	
 	# ignore spaces?
 	if ignore_spaces:
-		strlen = len(s_filtered2)
+		s_filtered = re.sub(r"\s+", '', s_filtered)
 	else:
-		strlen = len(s_filtered)
+		s_filtered = re.sub(r"\s+", '_', s_filtered)
 
-	s_filtered = s_filtered2
+
+	
+	strlen = len(s_filtered)
 			
 	if os.path.isfile(ssteps):
 		with open(ssteps, 'rt') as f:
@@ -37,16 +57,42 @@ def stepcount(ssteps, scanrate, ignore_spaces ,sentence):
 				letterfreq[row['Letter']]=row['Scan Steps']
 		# Now we have the letter freq chart lets do our sums on this with our test sentences
 		for n in s_filtered:
-			sum = sum + int(letterfreq[n])
+			sum = sum + float(letterfreq[n])
+			t_lesher = t_lesher + ((float(letterfreq[n])+2)*scanrate)
+			t_damper = t_damper + ((float(letterfreq[n])+1)*scanrate)
+			show_workings = show_workings + n +' ' + letterfreq[n] + ', '
+		
+		if ignore_predictions == False :
+			t_lesher = t_lesher + sum_pred_words * prediction_time
+			t_damper = t_damper + sum_pred_words * prediction_time
+			show_workings = show_workings +  ' AND add sum of predicted words ('+ str(+sum_pred_words)+') * prediction_time (' + str(prediction_time) + ')'
 				
-		print('Full steps:'+str(sum))
-		# ss  100 (nb  na)/nb
-		# nb - scan steps. na = augmented switch count.
-		print('Switch hits:'+str(strlen*2))
-		# Damper: 1/2 per switch hit. Takes two hits per letter so just length of str
-		print('Damper time (H:M:S.ms):'+mstotime((sum+strlen)*scanrate))
-		print('Lesher time (H:M:S.ms):'+mstotime((sum+(strlen*2))*scanrate))
-		print('No hit time (H:M:S.ms):'+mstotime((sum)*scanrate))		
+		if ('csv-all' in output_type):
+			# print a csv of all data.. 
+			# headers = 'steps, hits, lesher (ms), damper (ms), no-hit (ms), predicted words, predicted letters, wordcount
+			csv_line = str(sum) + ',' + str(strlen*2) + ',' + str(t_lesher) + ',' + str(t_damper) + ',' + str((sum)*scanrate) + ',' + str(sum_pred_words) + ',' + str(sum_pred_letters) + ',' + str(sum_words)
+			print(csv_line)			
+		
+		if ('Stats' in output_type or output_type == 'all'):		
+			print('Full steps:'+str(sum))
+			# ss  100 (nb  na)/nb
+			# nb - scan steps. na = augmented switch count.
+			# 2 hits per letter. Its always that way
+			print('Switch hits (auto scan - auto start):'+str(strlen*2))
+		if (('Lesher' in output_type) or (output_type == 'all')):
+			print('Lesher time (H:M:S.ms), '+mstotime(t_lesher))
+		if ('Damper' in output_type or output_type == 'all'):
+			print('Damper time (H:M:S.ms), '+mstotime(t_damper))
+		if ('No-Hit' in output_type or output_type == 'all'):
+			print('No hit time (H:M:S.ms), '+mstotime((sum)*scanrate))
+		if ('show-predictions' in output_type or output_type == 'all'):
+			print('No. of predicted words '+ str(sum_pred_words))
+			print('No. of predicted letters '+ str(sum_pred_letters))
+		if ('show-workings' in output_type or output_type == 'all'):
+			show_workings = 'string: ' + s_filtered + ' -> ' + show_workings + ' = Total steps of '+ str(sum) + '. Lesher Time = Σ (each step + 2) * scanrate (i.e. '+ str(scanrate) + ')'
+			if ignore_predictions == False :
+				show_workings = show_workings +  ' AND add sum of predicted words ('+ str(+sum_pred_words)+') * prediction_time (' + str(prediction_time) + ')'
+			print('Show workings:'+show_workings)
 	else:
 		return None
 		
--- TotalStepAnalysis.py
+++ TotalStepAnalysis.py
@@ -1,78 +1,121 @@
+import subprocess, csv
 import click
-import csv
 import os.path
-import re
-
-def mstotime(miliseconds):
-	hours, milliseconds = divmod(miliseconds, 3600000)
-	minutes, milliseconds = divmod(miliseconds, 60000)
-	seconds = float(milliseconds) / 1000
-	s = "%i:%02i:%06.3f" % (hours, minutes, seconds)
-	return s
-
 
 @click.command()
-@click.option('--ssteps', type=click.Path(exists=True), default='scan-steps-lib/ssteps-eardu.csv', help='Path to a csv of your scan steps')
+@click.option('--text-file', default='', help='name of text file to input')
+@click.option('--layouts', default='all', help='Which layouts do you want parsing? all, aeiou, abcd, db, eardu, qwerty, dbfreq')
+@click.option('--text', default="""the quick brown fox jumped over the lazy dog""", help='a line of text to analyse. NB: ignored if text file provided')
 @click.option('--scanrate', default=1000, help='Scan rate in ms')
-@click.option('--vocab-file', type=click.Path(exists=True), default='examples/test.txt', help='Path to your vocab file')
-@click.option('--ignore-spaces', default=True, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
+@click.option('--ignore-spaces', default=False, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
+@click.option('--ignore-predictions', default=True, type=bool, help='Ignore predictions? i.e. ignore anything uppercased')
+@click.option('--prediction-time', default=500, help='How long does it take the person to select a prediction on average? in ms. NB: Ignored if ignored-predictions is True')
 
-def getTotalVocabTime(ssteps, scanrate, ignore_spaces , vocab_file):
-	"""Takes a switch step count and a vocab file and display number of secs to get there"""
-	letterfreq = dict()
-	file = open(vocab_file, "r")
-	lines = file.readlines()
-	predicted_words = 1
-	words_per_sentence = []
-	pwords = dict()
-	full_steps = 0
-	switch_hits = 0
-	damper_time = 0
-	leshher_time = 0
-	nohit_time = 0
+def mstotime(ms):
+	ms = float(ms)
+	s=ms/1000
+	m,s=divmod(s,60)
+	h,m=divmod(m,60)
+	d,h=divmod(h,24)
+
+	return ("%d:%d:%d:%d" % (d, h, m, s))
 
 
-	with open(ssteps, 'rt') as f:
-		reader = csv.DictReader(f)
-		for row in reader:
-			letterfreq[row['Letter']]=row['Scan Steps']
-	
-	
-	for sentence in lines:		
-		# remove dodgy chars
-		# Bad code. Could do this a lot better if I spent 5 minutes 
-		s_filtered = re.sub(r"[0-9]+", '', sentence.lower())
-		s_filtered = re.sub(r"#+", '', s_filtered)
-		s_filtered = re.sub(r"'+", '', s_filtered)
-		s_filtered = re.sub(r"\/+", '', s_filtered)
-		s_filtered2 = re.sub(r"\s+", '', s_filtered)
+def textAnalyse(text_file, layouts, text, scanrate, ignore_spaces , ignore_predictions, prediction_time):
+	python_bin = '/usr/local/bin/python3'
+	base_cmd = python_bin + '  StepAnalysis.py --ssteps scan-steps-lib/ssteps-nnnn.csv --ignore-spaces '+str(ignore_spaces)+' --ignore-predictions '+str(ignore_predictions)+' --prediction-time '+str(prediction_time)+' --output-type csv-all --sentence '
 
-		# ignore spaces?
-		if ignore_spaces:
-			strlen = len(s_filtered2)
+	if text_file != '':
+		if os.path.exists(text_file):
+			file = open(text_file, "r")
+			text = file.read()
 		else:
-			strlen = len(s_filtered)
+			print('!!! Sorry. File doesnt exist')
 
-		s_filtered = s_filtered2
-			
-		# Now we have the letter freq chart lets do our sums on this with our test sentences
-		sum = 0
-		for n in s_filtered:
-			sum = sum + int(letterfreq[n])
-				
-		full_steps +=sum 
-		# ss  100 (nb  na)/nb
-		# nb - scan steps. na = augmented switch count.
-		switch_hits += strlen*2
-		# Damper: 1/2 per switch hit. Takes two hits per letter so just length of str
-		damper_time = damper_time + ((sum+strlen)*scanrate)
-		leshher_time = leshher_time + ((sum+(strlen*2))*scanrate)
-		nohit_time = nohit_time + ((sum)*scanrate)
+	lines = text.splitlines()
+	if (layouts == 'csv'):
+		print('line, predicted words, predicted letters, wordcount, hits,  abcd-steps,  abcd-lesher (ms), abcd-damper (ms), db-steps,  db-lesher (ms), db-damper (ms), dbfreq-lesher (ms), dbfreq-steps, dbfreq-damper (ms), dbfreqNS-lesher (ms), dbfreqNS-steps, dbfreqNS-damper (ms), qwerty-steps, qwerty-lesher (ms), qwerty-damper (ms),aeiou-steps, aeiou-lesher (ms), aeiou-damper (ms),eardu-steps, eardu-lesher (ms), eardu-damper (ms)')
+	else:
+		print('line, layout, steps, hits, lesher (ms), damper (ms), no-hit (ms), predicted words, predicted letters, wordcount')
 
-	print('Damper time (H:M:S.ms):'+mstotime(damper_time))
-	print('Lesher time (H:M:S.ms):'+mstotime(leshher_time))
-	print('No hit time (H:M:S.ms):'+mstotime(nohit_time))		
-	
+
+	for line in lines:
+		# send
+
+		# output a csv of the csv!
+		if (layouts == 'csv'):
+
+			abcd_cmd = base_cmd.replace("nnnn", "abcd")
+			abcd = subprocess.Popen(abcd_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			db_cmd = base_cmd.replace("nnnn", "db")
+			db = subprocess.Popen(db_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			dbfreq_cmd = base_cmd.replace("nnnn", "dbfreq")
+			dbfreq = subprocess.Popen(dbfreq_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			dbfreqns_cmd = base_cmd.replace("nnnn", "dbfreq-nospace")
+			dbfreqns = subprocess.Popen(dbfreqns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			qwerty_cmd = base_cmd.replace("nnnn", "qwerty")
+			qwerty = subprocess.Popen(qwerty_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			eardu_cmd = base_cmd.replace("nnnn", "eardu")
+			eardu = subprocess.Popen(eardu_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			aeiou_cmd = base_cmd.replace("nnnn", "aeiou")
+			aeiou = subprocess.Popen(aeiou_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+
+			#steps - 0, hits 1 , lesher (ms) 2, damper (ms) 3, no-hit (ms) 4, predicted words 5, predicted letters 6, wordcount 7
+			#'line, predicted words, predicted letters, wordcount, hits,  abcd-steps,  abcd-lesher (ms), abcd-damper (ms), db-steps,  db-lesher (ms), db-damper (ms), dbfreq-steps, dbfreq-lesher (ms), dbfreq-damper (ms), dbfreqNS-steps, dbfreqNS-lesher (ms), dbfreqNS-damper (ms), qwerty-steps, qwerty-lesher (ms), qwerty-damper (ms),aeiou-steps, aeiou-lesher (ms), aeiou-damper (ms),eardu-steps, eardu-lesher (ms), eardu-damper (ms)'
+
+			print(line+','+abcd[5]+','+abcd[6]+','+abcd[7]+','+abcd[1]
+			+','+abcd[0]+','+abcd[2]+','+abcd[3]
+			+','+db[0]+','+db[2]+','+db[3]
+			+','+dbfreq[0]+','+dbfreq[2]+','+dbfreq[3]
+			+','+dbfreqns[0]+','+dbfreqns[2]+','+dbfreqns[3]
+			+','+qwerty[0]+','+qwerty[2]+','+qwerty[3]
+			+','+aeiou[0]+','+aeiou[2]+','+aeiou[3]
+			+','+eardu[0]+','+eardu[2]+','+eardu[3])
+
+			'''
+						print(line+','+abcd[5]+','+abcd[6]+','+abcd[7]+','+abcd[1]
+						+','+abcd[0]+','+mstotime(abcd[2])+','+mstotime(abcd[3])
+						+','+db[0]+','+mstotime(db[2])+','+mstotime(db[3])
+						+','+dbfreq[0]+','+mstotime(dbfreq[2])+','+mstotime(dbfreq[3])
+						+','+dbfreqns[0]+','+mstotime(dbfreqns[2])+','+mstotime(dbfreqns[3])
+						+','+qwerty[0]+','+mstotime(qwerty[2])+','+mstotime(qwerty[3])
+						+','+aeiou[0]+','+mstotime(aeiou[2])+','+mstotime(aeiou[3])
+						+','+eardu[0]+','+mstotime(eardu[2])+','+mstotime(eardu[3]))
+			'''
+
+		else:
+
+			if (('abcd' in layouts) or (layouts == 'all')):
+				abcd_cmd = base_cmd.replace("nnnn", "abcd")
+				abcd = subprocess.Popen(abcd_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+				print(line+',abcd,'+abcd.strip())
+			if (('eardu' in layouts) or (layouts == 'all')):
+				eardu_cmd = base_cmd.replace("nnnn", "eardu")
+				eardu = subprocess.Popen(eardu_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+				print(line+',eardu,'+eardu.strip())
+			if (('aeiou' in layouts) or (layouts == 'all')):
+				aeiou_cmd = base_cmd.replace("nnnn", "aeiou")
+				aeiou = subprocess.Popen(aeiou_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+				print(line+',aeiou,'+aeiou.strip())
+			if (('db' in layouts) or (layouts == 'all')):
+				db_cmd = base_cmd.replace("nnnn", "db")
+				db = subprocess.Popen(db_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+				print(line+',db,'+db.strip())
+			if (('dbfreq' in layouts) or (layouts == 'all')):
+				dbfreq_cmd = base_cmd.replace("nnnn", "dbfreq")
+				db = subprocess.Popen(dbfreq_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+				print(line+',dbfreq,'+dbfreq.strip())
+			if (('qwerty' in layouts) or (layouts == 'all')):
+				qwerty_cmd = base_cmd.replace("nnnn", "qwerty")
+				qwerty = subprocess.Popen(qwerty_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8")
+				print(line+',qwerty,'+qwerty.strip())
 
 if __name__ == '__main__':
-    getTotalVocabTime()
+    textAnalyse()
