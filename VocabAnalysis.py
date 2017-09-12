eb865736cd7b7e8fae1232cfc94fe35528e31a1a
will wade <willwade@gmail.com>
2017-09-13 00:01:18 +0100

    fixed the analysis of spaces

--- VocabAnalysis.py
+++ VocabAnalysis.py
@@ -3,9 +3,9 @@ import csv
 import click
 import os.path
 
-def word_stats(file_name):
+def word_stats(file_name, ignore_spaces):
 	import re
-	# balladin core_word_list 
+	# balladin core_word_list
 	core_words = ["a", "can", "about", "cant", "actually", "car", "after", "catholic", "afternoon", "cause", "again", "close", "ago", "cold", "ah", "come", "all", "coming", "alright", "could", "always", "couldnt", "an", "couple", "and", "crew", "another", "day", "any", "days", "anything", "dear", "anyway", "did", "are", "didnt", "around", "different", "as", "do", "ask", "does", "at", "doesnt", "away", "doing", "back", "dollars", "bad", "done", "be", "dont", "beautiful", "down", "because", "dunno", "been", "eat", "before", "eight", "being", "either", "better", "eleven", "big", "else", "bit", "end", "bloody", "enough", "break", "er", "but", "even", "buy", "ever", "by", "every", "cake", "everyone", "came", "everything", "ey", "hour", "fair", "hours", "feel", "house", "find", "how", "finished", "hundred", "first", "i", "five", "if", "for", "in", "four", "into", "friday", "is", "from", "isnt", "fucking", "it", "get", "its", "gets", "its", "getting", "id", "give", "ill", "go", "im", "god", "ive", "goes", "job", "going", "just", "gone", "keep", "gonna", "kids", "good", "know", "got", "last", "gotta", "left", "had", "like", "half", "little", "happened", "live", "hard", "long", "has", "look", "have", "looking", "havent", "looks", "having", "lost", "he", "lot", "her", "love", "here", "lovely", "hell", "lunch", "hes", "made", "him", "make", "his", "many", "home", "married", "me", "people", "mean", "person", "might", "phone", "mind", "pick", "mine", "place", "minutes", "play", "mm", "pretty", "monday", "probably", "money", "put", "months", "quite", "more", "ready", "morning", "really", "much", "remember", "mum", "right", "must", "road", "my", "round", "name", "said", "need", "same", "never", "saturday", "new", "say", "next", "saying", "nice", "says", "night", "school", "no", "see", "not", "seen", "nothing", "seven", "now", "she", "of", "shell", "off", "shes", "oh", "shit", "ok", "shoes", "old", "should", "on", "sit", "once", "six", "one", "so", "ones", "some", "only", "someone", "or", "something", "other", "sorry", "our", "sort", "out", "start", "over", "started", "own", "still", "oclock", "straight", "past", "street", "pay", "stuff", "sure", "us", "take", "used", "talk", "very", "talking", "want", "tea", "wanted", "tell", "wants", "ten", "was", "than", "wasnt", "thanks", "way", "that", "we", "thats", "week", "the", "weekend", "their", "weeks", "them", "well", "then", "went", "there", "were", "theres", "were", "these", "weve", "they", "what", "theyre", "whats", "theyve", "when", "thing", "where", "things", "which", "think", "who", "thirty", "whos", "this", "why", "those", "will", "though", "with", "thought", "won", "three", "wont", "through", "work", "til", "working", "time", "would", "times", "wouldnt", "to", "wrong", "today", "yeah", "told", "year", "tomorrow", "years", "too", "yep", "try", "yes", "trying", "yesterday", "twelve", "you", "twenty", "your", "two", "youre", "um", "youve", "up"]
 	file = open(file_name, "r")
 	lines = file.readlines()
@@ -22,8 +22,8 @@ def word_stats(file_name):
 					if worda.lower() in in_core:
 						in_core[worda.lower()]=in_core[worda.lower()]+1
 					else:
-						in_core[worda.lower()]=1						
-				# Predicted? 		
+						in_core[worda.lower()]=1
+				# Predicted?
 				if any(d.isupper() for d in worda):
 					if worda.lower() in pwords:
 						pwords[worda.lower()]=pwords[worda.lower()]+1
@@ -37,22 +37,26 @@ def word_stats(file_name):
 	file = open(file_name, "r")
 	chars = file.read()
 	# remove dodgy chars
-	# Bad code. Could do this a lot better if I spent 5 minutes 
-	chars_filtered = re.sub(r"\s+", '', chars.lower())
-	chars_filtered = re.sub(r"[0-9]+", '', chars_filtered)
+	# Bad code. Could do this a lot better if I spent 5 minutes
+	chars_filtered = re.sub(r"[0-9]+", '', chars.lower())
 	chars_filtered = re.sub(r"#+", '', chars_filtered)
 	chars_filtered = re.sub(r"'+", '', chars_filtered)
 	chars_filtered = re.sub(r"\/+", '', chars_filtered)
-	
+	# ignore spaces?
+	if ignore_spaces:
+		chars_filtered = re.sub(r"\s+", '', chars_filtered)
+	else:
+		chars_filtered = re.sub(r"\s+", '_', chars_filtered)
+
 	# Word count
 	words = re.findall('\w+', chars.lower())
 	avgWordLen = sum(map(len, words))/len(words)
 	from collections import Counter
-	charcount = Counter(chars_filtered)	
+	charcount = Counter(chars_filtered)
 	wordcount = Counter(words)
 	return int(avgWordLen), charcount, wordcount, avg_wps, max_wps, predicted_words, pwords, in_core
 
-	
+
 # Ignore all letters after an uppercase character
 def getCharsSpoken(wordlist):
 	cl = list()
@@ -70,20 +74,26 @@ def removeStopwords(wordlist, stopwords):
 
 @click.command()
 @click.option('--vocab-file', type=click.Path(exists=True), default='examples/vocab.txt', help='Path to a vocab file. String on each line. ')
-#@click.option('--scan_steps', default=scan_steps, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
-@click.option('--inc-spaces', default=True, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
-#@click.option('--stop-words', default=True, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
+@click.option('--ignore-spaces', default=True, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
+@click.option('--ssteps', type=click.Path(exists=True), default='scan-steps-lib/ssteps-eardu.csv', help='Path to a csv of your scan steps')
+@click.option('--stop-words', default=False, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
 
-
-# Open file. 
+# Open file.
 #  NB: Each line is a sentence. Only uppercase letters which have been predicted by a partner - INCLUDING first letters
-def printStats(vocab_file, inc_spaces):
+def printStats(vocab_file, ignore_spaces, ssteps, stop_words):
 
 	# Outline your scan steps here
 	# Sams Scan Steps - with the extra hit
 	#scan_steps = {'e':2,'t':4,'o':7,'a':3,'n':9,'i':8,'s':5,'r':6,'h':4,'l':3,'d':8,'u':9,'w':4,'f':9,'m':5,'c':5,'g':7,'p':6,'b':8,'y':7,'k':6,'v':10,'x':6,'j':11,'z':7,'q':5}
 	# Heidis Scan steps - "Full" steps
-	scan_steps = { 'e':0,'t':2,'o':5,'a':1,'n':7,'i':6,'s':3,'r':4,'h':2,'l':1,'d':6,'u':7,'w':2,'f':7,'m':3,'c':3,'g':5,'p':4,'b':6,'y':5,'k':4,'v':8,'x':4,'j':9,'z':5,'q':3}
+	#scan_steps = { 'e':0,'t':2,'o':5,'a':1,'n':7,'i':6,'s':3,'r':4,'h':2,'l':1,'d':6,'u':7,'w':2,'f':7,'m':3,'c':3,'g':5,'p':4,'b':6,'y':5,'k':4,'v':8,'x':4,'j':9,'z':5,'q':3}
+	scan_steps = dict()
+	if os.path.isfile(ssteps):
+		with open(ssteps, 'rt') as f:
+			reader = csv.DictReader(f)
+			for row in reader:
+				scan_steps[row['Letter']]=row['Scan Steps']
+
 
 	# Not using these - but we could use this to exclude common (core) words
 	# e.g http://ir.dcs.gla.ac.uk/resources/linguistic_utils/stop_words
@@ -92,7 +102,7 @@ def printStats(vocab_file, inc_spaces):
 	stopwords += ['again', 'against', 'all', 'almost', 'alone', 'along']
 
 	total = totalpred = totalin_core = totalchr = 0
-	avgWordLen, charcount, wordcount, avg_wps, max_wps, predicted_words, pwords, in_core = word_stats(vocab_file)
+	avgWordLen, charcount, wordcount, avg_wps, max_wps, predicted_words, pwords, in_core = word_stats(vocab_file, ignore_spaces)
 	print("total words:")
 	with open('output-all-words.csv', 'w') as csv_file:
 		writer = csv.writer(csv_file)
@@ -120,7 +130,7 @@ def printStats(vocab_file, inc_spaces):
 	print("total chars:")
 	with open('output-incore-words.csv', 'w') as csv_file:
 		writer = csv.writer(csv_file)
-		for itemch in sorted(charcount, key=charcount.get, reverse=True): 
+		for itemch in sorted(charcount, key=charcount.get, reverse=True):
 			writer.writerow([itemch, charcount[itemch]])
 			print(itemch, charcount[itemch])
 			totalchr = totalchr+charcount[itemch]
@@ -138,12 +148,11 @@ def printStats(vocab_file, inc_spaces):
 	with open('output-freqfinal-words.csv', 'w') as csv_file:
 		writer = csv.writer(csv_file)
 		writer.writerow(['Letter count', 'Frequency', 'Letter', 'Scan Steps', 'FreqXScanSteps' ])
-		for s in charcount: 
-			writer.writerow([str(float(charcount[s])),str(float(charcount[s])/totalchr),s, scan_steps[s], str((float(charcount[s])/totalchr) * scan_steps[s])])
-			print(str(float(charcount[s])),',',str(float(charcount[s])/totalchr),',',s,',', scan_steps[s],',', str((float(charcount[s])/totalchr) * scan_steps[s]))
-			totalFreqScan = totalFreqScan + ((float(charcount[s])/totalchr) * scan_steps[s])
+		for s in charcount:
+			writer.writerow([str(float(charcount[s])),str(float(charcount[s])/totalchr),s, scan_steps[s], str((float(charcount[s])/totalchr) * int(scan_steps[s]))])
+			print(str(float(charcount[s])),',',str(float(charcount[s])/totalchr),',',s,',', scan_steps[s],',', str((float(charcount[s])/totalchr) * int(scan_steps[s])))
+			totalFreqScan = totalFreqScan + ((float(charcount[s])/totalchr) * int(scan_steps[s]))
 	print("Total Frequency X Scan Steps:", totalFreqScan)
 
 if __name__ == '__main__':
 	printStats()
-
