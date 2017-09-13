2f4fc064af3cf5edd564860c4c53ee72cefe0270
will wade <willwade@gmail.com>
2017-09-13 07:20:28 +0100

    made to run quicker for the nospace versions of files

--- TotalStepAnalysis.py
+++ TotalStepAnalysis.py
@@ -2,15 +2,6 @@ import subprocess, csv
 import click
 import os.path
 
-@click.command()
-@click.option('--text-file', default='', help='name of text file to input')
-@click.option('--layouts', default='all', help='Which layouts do you want parsing? all, aeiou, abcd, db, eardu, qwerty, dbfreq')
-@click.option('--text', default="""the quick brown fox jumped over the lazy dog""", help='a line of text to analyse. NB: ignored if text file provided')
-@click.option('--scanrate', default=1000, help='Scan rate in ms')
-@click.option('--ignore-spaces', default=False, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
-@click.option('--ignore-predictions', default=True, type=bool, help='Ignore predictions? i.e. ignore anything uppercased')
-@click.option('--prediction-time', default=500, help='How long does it take the person to select a prediction on average? in ms. NB: Ignored if ignored-predictions is True')
-
 def mstotime(ms):
 	ms = float(ms)
 	s=ms/1000
@@ -21,9 +12,19 @@ def mstotime(ms):
 	return ("%d:%d:%d:%d" % (d, h, m, s))
 
 
-def textAnalyse(text_file, layouts, text, scanrate, ignore_spaces , ignore_predictions, prediction_time):
+@click.command()
+@click.option('--text-file', default='', help='name of text file to input')
+@click.option('--layouts', default='all', help='Which layouts do you want parsing? all, aeiou, abcd, db, eardu, qwerty, dbfreq')
+@click.option('--text', default="""the quick brown fox jumped over the lazy dog""", help='a line of text to analyse. NB: ignored if text file provided')
+@click.option('--scanrate', default=1000, help='Scan rate in ms')
+@click.option('--ignore-spaces', default=False, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
+@click.option('--remove-predicted', default=False, type=bool, help='Remove predicted letters? i.e. ignore anything uppercased')
+@click.option('--prediction-time', default=500, help='How long does it take the person to select a prediction on average? in ms. NB: Ignored if ignored-predictions is True')
+
+
+def textAnalyse(text_file, layouts, text, scanrate, ignore_spaces , remove_predicted, prediction_time):
 	python_bin = '/usr/local/bin/python3'
-	base_cmd = python_bin + '  StepAnalysis.py --ssteps scan-steps-lib/ssteps-nnnn.csv --ignore-spaces '+str(ignore_spaces)+' --ignore-predictions '+str(ignore_predictions)+' --prediction-time '+str(prediction_time)+' --output-type csv-all --sentence '
+	base_cmd = python_bin + '  StepAnalysis.py --ssteps scan-steps-lib/ssteps-nnnn.csv --ignore-spaces '+str(ignore_spaces)+' --remove-predicted '+str(remove_predicted)+' --prediction-time '+str(prediction_time)+' --output-type csv-all --sentence '
 
 	if text_file != '':
 		if os.path.exists(text_file):
@@ -33,16 +34,38 @@ def textAnalyse(text_file, layouts, text, scanrate, ignore_spaces , ignore_predi
 			print('!!! Sorry. File doesnt exist')
 
 	lines = text.splitlines()
+	if (layouts == 'csvns'):
+		print('line, predicted words, predicted letters, wordcount, hits,  dbNS-steps,  dbNS-lesher (ms), dbNS-damper (ms), dbfreqNS-steps, dbfreqNS-lesher, dbfreqNS-damper (ms), qwerty-steps, qwerty-lesher (ms), qwerty-damper (ms),eardu-steps, eardu-lesher (ms), eardu-damper (ms)')
+
 	if (layouts == 'csv'):
-		print('line, predicted words, predicted letters, wordcount, hits,  abcd-steps,  abcd-lesher (ms), abcd-damper (ms), db-steps,  db-lesher (ms), db-damper (ms), dbfreq-lesher (ms), dbfreq-steps, dbfreq-damper (ms), dbfreqNS-lesher (ms), dbfreqNS-steps, dbfreqNS-damper (ms), qwerty-steps, qwerty-lesher (ms), qwerty-damper (ms),aeiou-steps, aeiou-lesher (ms), aeiou-damper (ms),eardu-steps, eardu-lesher (ms), eardu-damper (ms)')
+		print('line, predicted words, predicted letters, wordcount, hits,  abcd-steps,  abcd-lesher (ms), abcd-damper (ms), db-steps,  db-lesher (ms), db-damper (ms), dbNS-steps,  dbNS-lesher (ms), dbNS-damper (ms), dbfreq-steps, dbfreq-lesher (ms), dbfreq-damper (ms), dbfreqNS-steps, dbfreqNS-lesher, dbfreqNS-damper (ms), qwerty-steps, qwerty-lesher (ms), qwerty-damper (ms), qwertyNS-steps, qwertyNS-lesher (ms), qwertyNS-damper (ms), aeiou-steps, aeiou-lesher (ms), aeiou-damper (ms),eardu-steps, eardu-lesher (ms), eardu-damper (ms), earduNS-steps, earduNS-lesher (ms), earduNS-damper (ms)')
 	else:
 		print('line, layout, steps, hits, lesher (ms), damper (ms), no-hit (ms), predicted words, predicted letters, wordcount')
 
 
 	for line in lines:
 		# send
-
 		# output a csv of the csv!
+		if (layouts == 'csvns'):
+
+			dbns_cmd = base_cmd.replace("nnnn", "db-nospace")
+			dbns = subprocess.Popen(dbns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			dbfreqns_cmd = base_cmd.replace("nnnn", "dbfreq-nospace")
+			dbfreqns = subprocess.Popen(dbfreqns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			qwertyns_cmd = base_cmd.replace("nnnn", "qwerty-nospace")
+			qwertyns = subprocess.Popen(qwertyns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			earduns_cmd = base_cmd.replace("nnnn", "eardu-nospace")
+			earduns = subprocess.Popen(earduns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			print(line+','+dbns[5]+','+dbns[6]+','+dbns[7]+','+dbns[1]
+			+','+dbns[0]+','+dbns[2]+','+dbns[3]
+			+','+dbfreqns[0]+','+dbfreqns[2]+','+dbfreqns[3]
+			+','+qwertyns[0]+','+qwertyns[2]+','+qwertyns[3]
+			+','+earduns[0]+','+earduns[2]+','+earduns[3])
+
 		if (layouts == 'csv'):
 
 			abcd_cmd = base_cmd.replace("nnnn", "abcd")
@@ -50,6 +73,9 @@ def textAnalyse(text_file, layouts, text, scanrate, ignore_spaces , ignore_predi
 
 			db_cmd = base_cmd.replace("nnnn", "db")
 			db = subprocess.Popen(db_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			dbns_cmd = base_cmd.replace("nnnn", "db-nospace")
+			dbns = subprocess.Popen(dbns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
 
 			dbfreq_cmd = base_cmd.replace("nnnn", "dbfreq")
 			dbfreq = subprocess.Popen(dbfreq_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
@@ -60,8 +86,14 @@ def textAnalyse(text_file, layouts, text, scanrate, ignore_spaces , ignore_predi
 			qwerty_cmd = base_cmd.replace("nnnn", "qwerty")
 			qwerty = subprocess.Popen(qwerty_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
 
+			qwertyns_cmd = base_cmd.replace("nnnn", "qwerty-nospace")
+			qwertyns = subprocess.Popen(qwertyns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
 			eardu_cmd = base_cmd.replace("nnnn", "eardu")
 			eardu = subprocess.Popen(eardu_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
+
+			earduns_cmd = base_cmd.replace("nnnn", "eardu-nospace")
+			earduns = subprocess.Popen(earduns_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
 
 			aeiou_cmd = base_cmd.replace("nnnn", "aeiou")
 			aeiou = subprocess.Popen(aeiou_cmd + '"'+line+'"', shell=True, stdout=subprocess.PIPE).stdout.read().decode("utf-8").strip().split(',')
@@ -73,21 +105,25 @@ def textAnalyse(text_file, layouts, text, scanrate, ignore_spaces , ignore_predi
 			print(line+','+abcd[5]+','+abcd[6]+','+abcd[7]+','+abcd[1]
 			+','+abcd[0]+','+abcd[2]+','+abcd[3]
 			+','+db[0]+','+db[2]+','+db[3]
+			+','+dbns[0]+','+dbns[2]+','+dbns[3]
 			+','+dbfreq[0]+','+dbfreq[2]+','+dbfreq[3]
 			+','+dbfreqns[0]+','+dbfreqns[2]+','+dbfreqns[3]
 			+','+qwerty[0]+','+qwerty[2]+','+qwerty[3]
+			+','+qwertyns[0]+','+qwertyns[2]+','+qwertyns[3]
 			+','+aeiou[0]+','+aeiou[2]+','+aeiou[3]
-			+','+eardu[0]+','+eardu[2]+','+eardu[3])
+			+','+eardu[0]+','+eardu[2]+','+eardu[3]
+			+','+earduns[0]+','+earduns[2]+','+earduns[3])
+
 
 			'''
-						print(line+','+abcd[5]+','+abcd[6]+','+abcd[7]+','+abcd[1]
-						+','+abcd[0]+','+mstotime(abcd[2])+','+mstotime(abcd[3])
-						+','+db[0]+','+mstotime(db[2])+','+mstotime(db[3])
-						+','+dbfreq[0]+','+mstotime(dbfreq[2])+','+mstotime(dbfreq[3])
-						+','+dbfreqns[0]+','+mstotime(dbfreqns[2])+','+mstotime(dbfreqns[3])
-						+','+qwerty[0]+','+mstotime(qwerty[2])+','+mstotime(qwerty[3])
-						+','+aeiou[0]+','+mstotime(aeiou[2])+','+mstotime(aeiou[3])
-						+','+eardu[0]+','+mstotime(eardu[2])+','+mstotime(eardu[3]))
+			print(line+','+abcd[5]+','+abcd[6]+','+abcd[7]+','+abcd[1]
+			+','+abcd[0]+','+mstotime(abcd[2])+','+mstotime(abcd[3])
+			+','+db[0]+','+mstotime(db[2])+','+mstotime(db[3])
+			+','+dbfreq[0]+','+mstotime(dbfreq[2])+','+mstotime(dbfreq[3])
+			+','+dbfreqns[0]+','+mstotime(dbfreqns[2])+','+mstotime(dbfreqns[3])
+			+','+qwerty[0]+','+mstotime(qwerty[2])+','+mstotime(qwerty[3])
+			+','+aeiou[0]+','+mstotime(aeiou[2])+','+mstotime(aeiou[3])
+			+','+eardu[0]+','+mstotime(eardu[2])+','+mstotime(eardu[3]))
 			'''
 
 		else:
