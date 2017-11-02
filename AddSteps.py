a5fd1bd3bf56fcff825ede0ee84d412b70786892
willwade <willwade@gmail.com>
2017-11-02 09:38:31 +0000

    addition of phrase based calc scripts

---
+++ AddSteps.py
@@ -0,0 +1,83 @@
+'''
+	AddSteps.py --ssteps scan-steps-lib/ssteps-db.csv --freqtable output-all-words.csv --write-to-col 'spell:db orig' --addspace True
+	
+	AddSteps.py --ssteps scan-steps-lib/ssteps-phrase-6x6.csv --sstep-type Phrases --freqtable output-all-words.csv --write-to-col 'phrase:6x6'
+	
+	## What this  does?
+	
+	Takes in a scan step csv chart (letter/phrase|steps\n) and a frequency list (phrase|frequency(count)\n) and adds a new column to the frequency chart of the steps to the steps to write the phrase with this scan step layout.
+	
+	Note use add space to emulate adding a space at the end of each phrase. Your scan step chart will need to have a _ character in it however
+	
+	
+'''
+import sys
+import csv
+import click
+import os.path
+import re
+from collections import OrderedDict
+
+def hasNumbers(inputString):
+	return bool(re.search(r'\d', inputString))
+
+@click.command()
+@click.option('--ssteps', type=click.Path(exists=True), default='scan-steps-lib/ssteps-eardu.csv', help='Path to a csv of your scan steps')
+@click.option('--freqtable', type=click.Path(exists=True), default='output-all-words.csv', help='Path to a csv of your words|frequency csv')
+@click.option('--sstep-type', default='Letters', help='Letters or Phrases')
+@click.option('--write-to-col', default='', help='Wamt to write the scan steps in a new column? Provide the string of the title here ')
+@click.option('--addspace', default=False, help='Add a space at the end of each phrase?')
+
+def stepcalc(ssteps,freqtable,sstep_type,write_to_col,addspace):
+	# Get Scan Steps
+	letterfreq = freqlist = dict()
+	if os.path.isfile(ssteps):
+		with open(ssteps, 'rt') as f:
+			reader = csv.DictReader(f)
+			for row in reader:
+				letterfreq[row['Letter']]=row['Scan Steps']
+
+	# Now get each word
+	# We cant read and write at same time 
+	new_rows_list = []
+	indexPhrase = 0
+	indexFreq = 1
+	
+	if os.path.isfile(freqtable):
+		with open(freqtable, "rt", encoding='utf-8') as csvfile:
+			reader = csv.reader(csvfile)
+			for i, row in enumerate(reader):
+				if (i == 0):
+					fieldnames = row
+					fieldnames.append(write_to_col)
+					if ('phrase' in fieldnames):
+						indexPhrase = fieldnames.index('phrase')
+					if ('freq' in fieldnames):
+						indexFreq = fieldnames.index('freq')						
+				r = 1
+				sum = 0
+				if (hasNumbers(row[indexPhrase])==False):
+					if sstep_type == 'Letters':
+						if (addspace):
+							row[indexPhrase] = row[indexPhrase] + '_'
+						for letter in row[indexPhrase]:
+							sum = sum + float(letterfreq[letter])
+					else:
+						if (row[indexPhrase] in letterfreq):
+							sum = float(letterfreq[row[indexPhrase]])
+				new_row = [row[indexPhrase], row[indexFreq], str(sum)]
+				new_rows_list.append(new_row)
+			csvfile.close()
+
+		# Now write it
+		with open(freqtable, 'w') as csvfile:
+			writer = csv.writer(csvfile,delimiter=',', lineterminator='\n')
+			for row in new_rows_list:
+				writer.writerow(row)
+			csvfile.close()
+	else:
+		return None
+
+
+if __name__ == '__main__':
+	stepcalc()
---
+++ MakeStepChart.py
@@ -0,0 +1,85 @@
+'''
+	MakeStepChart.py --columns 6 --rows 6
+	
+	## What this  does?
+	
+	Makes a step chart in a block of n x n cols and rows.
+	Then from frequency list and then applies this to finding the steps in this chart and writing the scan step lib
+	
+	Note: The provided list MUST be already sorted in frequency order
+	
+'''
+import sys
+import csv
+import click
+import os.path
+import re
+
+def makesteps(columns,rows,startat):
+	cell_size = columns * rows
+	table= [ [ 0 for i in range(columns) ] for j in range(rows) ]
+	stepcounts = []
+	for d1 in range(rows):
+		for d2 in range(columns):
+			stepcount = d1+d2+startat
+			table[d1][d2]= stepcount
+			stepcounts.append(stepcount)
+	
+	return table, cell_size, stepcounts
+
+
+def hasNumbers(inputString):
+	return bool(re.search(r'\d', inputString))
+
+@click.command()
+@click.option('--freqtable', type=click.Path(exists=True), default='output-all-words.csv', help='Path to a csv of your words|frequency csv')
+@click.option('--columns', default=6, help='Columns')
+@click.option('--rows', default=6, help='Rows')
+@click.option('--startat', default=2, help='What should the first element start at? 0? 1? 2?')
+@click.option('--newfile', default='scan-steps-lib/ssteps-phrase-nn.csv', help='Name the new file')
+@click.option('--ignore-let-less', default=30, help='ignore letters less than n length')
+
+
+
+def mapfreqtotable(freqtable,columns,rows,startat,newfile,ignore_let_less):
+
+	table, cell_size, stepcounts = makesteps(columns,rows,startat)
+	steprange = range(startat, max(int(num) for num in stepcounts))
+	stepcounts = sorted(stepcounts)
+	
+	# now lets work out where it should be.. what steps it would be if optimised
+	new_rows_list = new_row = old_rows_list = []
+
+	if os.path.isfile(freqtable):
+		with open(freqtable, "rt", encoding='utf-8') as csvfile:
+			reader = csv.reader(csvfile)
+			for i, line in enumerate(reader):
+				# this is a bit naff. Gets the header names
+				if (i == 0):
+					fieldnames = ['Letter','Scan Steps']
+				else:
+					# Ok - now loop through the nice little step matrix we made from makesteps
+					if (i<len(stepcounts)):
+						new_row = [line[0],stepcounts[i-1]]
+						new_rows_list.append(new_row)
+				
+		csvfile.close()
+		
+		# Now write the step count chart
+		if (newfile == 'scan-steps-lib/ssteps-phrase-nn.csv'):
+			newfile = newfile.replace('nn',str(columns)+'x'+str(rows)) 
+		
+		with open(newfile, 'w') as csvfile:
+			writer = csv.writer(csvfile,delimiter=',', lineterminator='\n')
+			writer.writerow(fieldnames)
+			for row in new_rows_list:
+				writer.writerow(row)
+			csvfile.close()
+
+		print ('Your scanning table will look like')	
+		for rrow in table:
+			print(' | '.join(str(e) for e in rrow))
+
+
+if __name__ == '__main__':
+	mapfreqtotable()
