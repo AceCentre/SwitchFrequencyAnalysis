592e8fd2f72117842e8de5edcfec6b7cbe84275b
willwade <willwade@gmail.com>
2018-01-25 00:47:15 +0000

    updated stepAnalysis to deal with unitTests and Incode calcs

--- StepAnalysis.py
+++ StepAnalysis.py
@@ -3,19 +3,7 @@ import csv
 import os.path
 import re
 
-@click.command()
-@click.option('--ssteps', type=click.Path(exists=True), default='scan-steps-lib/ssteps-eardu.csv', help='Path to a csv of your scan steps')
-@click.option('--ssteps-add', type=int, default=0, help='Increment the scan steps')
-@click.option('--ssteps-phrases', type=click.Path(), default='', help='Path to a csv of a second scan steps file. Should be whole words')
-@click.option('--scanrate', default=1000, help='Scan rate in ms')
-@click.option('--output-type', default='all', help='All, Lesher, Damper, Steps, Hits, show-workings, show-predictions, csv-all')
-@click.option('--ignore-spaces', default=True, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
-@click.option('--ignore-predicted', default=True, type=bool, help='Ignore predicted letters? i.e. anything uppercased gets lowercased and assumed he spoke it')
-@click.option('--remove-predicted', default=False, type=bool, help='Remove predicted letters? i.e. ignore anything uppercased')
-@click.option('--prediction-time', default=0, help='How long does it take the person to select a prediction on average? in ms. NB: Ignored if ignored-predictions is True')
-@click.option('--sentence', prompt='Test sentence:',help='Enter your test sentence here')
-
-def stepcount(ssteps, ssteps_add, ssteps_phrases, scanrate, ignore_spaces , ignore_predicted, remove_predicted, prediction_time, sentence, output_type):
+def stepcountCalc(ssteps, ssteps_add, ssteps_phrases, scanrate, ignore_spaces , ignore_predicted, remove_predicted, prediction_time, sentence):
 	"""Takes a switch step count and a sentence and display number of steps to get there"""
 	letterfreq = dict()
 	sum = t_lesher = t_damper = sum_pred_letters = sum_pred_words = sum_words = 0
@@ -112,35 +100,58 @@ def stepcount(ssteps, ssteps_add, ssteps_phrases, scanrate, ignore_spaces , igno
 			t_damper = t_damper + sum_pred_words * prediction_time
 			show_workings = show_workings +  ' AND add sum of predicted words ('+ str(+sum_pred_words)+') * prediction_time (' + str(prediction_time) + ')'
 
-		if ('csv-all' in output_type):
-			# print a csv of all data..
-			# headers = 'steps, hits, lesher (ms), damper (ms), no-hit (ms), predicted words, predicted letters, wordcount
-			csv_line = str(sum) + ',' + str(strlen*2) + ',' + str(t_lesher) + ',' + str(t_damper) + ',' + str((sum)*scanrate) + ',' + str(sum_pred_words) + ',' + str(sum_pred_letters) + ',' + str(sum_words)
-			print(csv_line)
-
-		if ('Stats' in output_type or output_type == 'all'):
-			print('Full steps:'+str(sum))
-			# ss  100 (nb  na)/nb
-			# nb - scan steps. na = augmented switch count.
-			# 2 hits per letter. Its always that way
-			print('Switch hits (auto scan - auto start):'+str(strlen*2+phrase_hits))
-		if (('Lesher' in output_type) or (output_type == 'all')):
-			print('Lesher time (H:M:S.ms), '+mstotime(t_lesher))
-		if ('Damper' in output_type or output_type == 'all'):
-			print('Damper time (H:M:S.ms), '+mstotime(t_damper))
-		if ('No-Hit' in output_type or output_type == 'all'):
-			print('No hit time (H:M:S.ms), '+mstotime((sum)*scanrate))
-		if ('show-predictions' in output_type or output_type == 'all'):
-			print('No. of predicted words '+ str(sum_pred_words))
-			print('No. of predicted letters '+ str(sum_pred_letters))
-		if ('show-workings' in output_type or output_type == 'all'):
-			show_workings = 'string: ' + s_filtered + ' -> ' + show_workings + ' = Total steps of '+ str(sum) + '.\n Lesher Time = Σ (each step + 2) * scanrate (i.e. '+ str(scanrate) + ')\n'
-			if remove_predicted == False :
-				show_workings = show_workings +  '               AND add sum of predicted words ('+ str(+sum_pred_words)+') * prediction_time (' + str(prediction_time) + ')'
-			print('Show workings:'+show_workings)
+		# Show workings	
+		show_workings = 'string: ' + s_filtered + ' -> ' + show_workings + ' = Total steps of '+ str(sum) + '.\n Lesher Time = Σ (each step + 2) * scanrate (i.e. '+ str(scanrate) + ')\n'
+		if remove_predicted == False :
+			show_workings = show_workings +  '               AND add sum of predicted words ('+ str(+sum_pred_words)+') * prediction_time (' + str(prediction_time) + ')'
+		
+		# Only do this. will return everything	
+		return sum, strlen*2 , t_lesher, t_damper, (sum)*scanrate , sum_pred_words , sum_pred_letters, sum_words, phrase_hits, show_workings			
 	else:
 		return None
 
+
+@click.command()
+@click.option('--ssteps', type=click.Path(exists=True), default='scan-steps-lib/ssteps-eardu.csv', help='Path to a csv of your scan steps')
+@click.option('--ssteps-add', type=int, default=0, help='Increment the scan steps')
+@click.option('--ssteps-phrases', type=click.Path(), default='', help='Path to a csv of a second scan steps file. Should be whole words')
+@click.option('--scanrate', default=1000, help='Scan rate in ms')
+@click.option('--output-type', default='all', help='All, Lesher, Damper, Steps, Hits, show-workings, show-predictions, csv-all')
+@click.option('--ignore-spaces', default=True, type=bool, help='Ignore spaces? Useful if you have no space in your layout')
+@click.option('--ignore-predicted', default=True, type=bool, help='Ignore predicted letters? i.e. anything uppercased gets lowercased and assumed he spoke it')
+@click.option('--remove-predicted', default=False, type=bool, help='Remove predicted letters? i.e. ignore anything uppercased')
+@click.option('--prediction-time', default=0, help='How long does it take the person to select a prediction on average? in ms. NB: Ignored if ignored-predictions is True')
+@click.option('--sentence', prompt='Test sentence:',help='Enter your test sentence here')
+
+def stepcount(ssteps, ssteps_add, ssteps_phrases, scanrate, ignore_spaces , ignore_predicted, remove_predicted, prediction_time, sentence, output_type):
+	steps, hits, lesher, damper, no_hit, predicted_words, predicted_letters, wordcount, phrase_hits, show_workings =	 stepcountCalc(ssteps, ssteps_add, ssteps_phrases, scanrate, ignore_spaces , ignore_predicted, remove_predicted, prediction_time, sentence)
+	
+	if ('code' == output_type):
+		# Only do this. will return everything	
+		return steps, hits, lesher, damper, no_hit, predicted_words, predicted_letters, wordcount, phrase_hits, show_workings
+		
+	if ('csv-all' in output_type):
+		# print a csv of all data..
+		# headers = 'steps, hits, lesher (ms), damper (ms), no-hit (ms), predicted words, predicted letters, wordcount
+		csv_line = str(steps)+ ','+ str(hits)+ ','+ str(lesher)+ ','+ str(damper)+ ','+ str(no_hit)+ ','+ str(predicted_words)+ ','+ str(predicted_letters)+ ','+ str(wordcount)
+		print(csv_line)
+	if ('Stats' in output_type or output_type == 'all'):
+		print('Full steps:'+str(steps))
+		# ss  100 (nb  na)/nb
+		# nb - scan steps. na = augmented switch count.
+		# 2 hits per letter. Its always that way
+		print('Switch hits (auto scan - auto start):'+str(hits+phrase_hits))
+	if (('Lesher' in output_type) or (output_type == 'all')):
+		print('Lesher time (H:M:S.ms), '+mstotime(lesher))
+	if ('Damper' in output_type or output_type == 'all'):
+		print('Damper time (H:M:S.ms), '+mstotime(damper))
+	if ('No-Hit' in output_type or output_type == 'all'):
+		print('No hit time (H:M:S.ms), '+mstotime((steps)*scanrate))
+	if ('show-predictions' in output_type or output_type == 'all'):
+		print('No. of predicted words '+ str(predicted_words))
+		print('No. of predicted letters '+ str(predicted_letters))
+	if ('show-workings' in output_type or output_type == 'all'):
+		print('Show workings:'+show_workings)
 
 def mstotime(miliseconds):
 	hours, milliseconds = divmod(miliseconds, 3600000)
--- VocabAnalysis_test.py
+++ VocabAnalysis_test.py
@@ -1,5 +1,6 @@
 import VocabAnalysis as va
 import CleanText 
+import StepAnalysis as sa 
 
 def test_word_stats():
 	avgWordLen, charcount, wordcount, avg_wps, max_wps, predicted_words, pwords, in_core, totals = va.word_stats('examples/vocab-cleaned.txt', False, True)
@@ -8,8 +9,30 @@ def test_word_stats():
 	assert avg_wps == 5.777777777777778
 	assert max_wps == 8
 	assert predicted_words == 4
-	assert totalFreqScan == 4.333333333333331
+	assert round(totalFreqScan, 3) == round(4.333333333333331, 3)
 
+def test_step_stats():
+	# Our Vars to test
+	sentence = 'the quick brown fox jumps over the lazy dog'
+	ssteps='scan-steps-lib/ssteps-abcd.csv'
+	ssteps_add=0
+	ssteps_phrases=''
+	scanrate=1000
+	ignore_spaces=True
+	ignore_predicted=False
+	remove_predicted=False
+	prediction_time=0 
+	output_type='code'
+	steps, hits, lesher, damper, no_hit, predicted_words, predicted_letters, wordcount, phrase_hits, show_workings = sa.stepcountCalc(ssteps, ssteps_add, ssteps_phrases, scanrate, ignore_spaces , ignore_predicted, remove_predicted, prediction_time, sentence)
+	#print(steps, hits, lesher, damper, no_hit, predicted_words, predicted_letters, wordcount, show_workings)
+	assert steps == 143.0
+	assert hits == 70 
+	assert lesher == 213000.0
+	assert damper == 178000.0
+	assert no_hit == 143000.0 
+	assert predicted_words == 0 
+	assert predicted_letters == 0 
+	assert wordcount == 9
 
 if __name__ == '__main__':
-	test_word_stats()
+	test_step_stats()
